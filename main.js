9b0f471945ce803f31c7ee8d042270b24f0e18dd
pabluchokk <pabloagusrivero17@gmail.com>
2023-07-18 21:33:09 -0300

    Corregidos algunos errores en los cálculos
    Algunas sumas superaban el presupuesto seleccionado.

--- main.js
+++ main.js
@@ -1,58 +1,61 @@
-const suma = (a,b) => a + b
-const resta = (a,b) => a - b
+const suma = (a, b) => a + b;
+const resta = (a, b) => a - b;
 
-let valorProducto = 0
-let presupuesto = 3000
-let valorFinal = 0
+let valorProducto = 0;
+let presupuesto = prompt("Ingrese su presupuesto")
+let valorFinal = 0;
 
-let aviso = alert(`El presupuesto máximo para gastar es de $${presupuesto}, no te excedas por favor...`)
-let producto = prompt(`Seleccione un producto. El producto A vale $500, El B vale $1000 y el C vale $1500.`)
+alert(`El presupuesto máximo para gastar es de $${presupuesto}. Por favor, no te excedas.`);
 
-switch (producto) {
+let producto = prompt(`Seleccione un producto. El producto A vale $500, el B vale $1000 y el C vale $1500.`);
+
+switch (producto.toLowerCase()) {
+case "a":
+    valorProducto = 500;
+    valorFinal = suma(valorProducto, valorFinal);
+    break;
+case "b":
+    valorProducto = 1000;
+    valorFinal = suma(valorProducto, valorFinal);
+    break;
+case "c":
+    valorProducto = 1500;
+    valorFinal = suma(valorProducto, valorFinal);
+    break;
+default:
+    alert("No se encontró el producto");
+    break;
+}
+
+if (valorFinal > presupuesto) {
+    alert("Te quedaste sin presupuesto");
+}
+
+producto = prompt(`Hay $${valorFinal} en el carrito. ¿Deseas agregar otro producto? Seleccione S/N`);
+
+while (producto.toLowerCase() === "s") {
+    producto = prompt(`Seleccione un producto. El producto A vale $500 ARS. El B vale $1000 ARS y el C vale $1500 ARS`);
+    let valorNuevoProducto = 0;
+
+    switch (producto.toLowerCase()) {
     case "a":
-        valorProducto = 500
-        valorFinal = suma (valorProducto, valorFinal)
+        valorNuevoProducto = 500;
         break;
     case "b":
-            valorProducto = 1000
-            valorFinal = suma (valorProducto, valorFinal)
-            break;
+        valorNuevoProducto = 1000;
+        break;
     case "c":
-        valorProducto = 1500
-        valorFinal = suma (valorProducto, valorFinal)
+        valorNuevoProducto = 1500;
         break;
     default:
-        alert("No se encontro el producto")
+        alert("No se encontró el producto");
         break;
-}
-if (valorFinal > presupuesto) {
-    alert("Te quedaste sin presupuesto")
-} 
-
-producto = prompt(`Hay $${valorFinal} en el carrito, desea realizar agregar otro producto? Seleccione S/N`)
-
-while (producto === "s") {
-    if (valorFinal >= presupuesto) {
-        alert("¡Te quedaste sin fondos!")
-    }  else {
-        let producto = prompt(`Seleccione un producto. El producto A vale $500 ARS. El B vale $1000 ARS y el C vale $1500 ARS`)
-    switch (producto) {
-        case "a":
-            valorProducto = 500
-            valorFinal = suma (valorProducto, valorFinal)
-            break;
-        case "b":
-                valorProducto = 1000
-                valorFinal = suma (valorProducto, valorFinal)
-                break;
-        case "c":
-            valorProducto = 1500
-            valorFinal = suma (valorProducto, valorFinal)
-            break;
-        default:
-            alert("No se encontro el producto")
-            break;
     }
-} 
-producto = prompt(`Hay $${valorFinal} en el carrito, desea comprar otro producto? Seleccione S/N`)
+
+    if (suma(valorNuevoProducto, valorFinal) > presupuesto) {
+        alert("No puedes agregar ese producto, excede tu presupuesto");
+    } else {
+        valorFinal = suma(valorNuevoProducto, valorFinal);
+    }
+    producto = prompt(`Hay $${valorFinal} en el carrito. ¿Deseas comprar otro producto? Seleccione S/N`);
 }
